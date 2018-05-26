f6a12643c822e49087f81f0ebbb801e05b8e67d2
Mihael Konjevic <konjevic@gmail.com>
2018-05-26 15:17:42 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/lein-template "0.1.3"
+(defproject keechma/lein-template "0.1.4"
   :description "Keechma Framework template"
   :url "http://keechma.com/"
   :license {:name "MIT"
